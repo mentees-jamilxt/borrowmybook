31bd3c3cc6d2751b841fab94b13dcd6132d80254
jamilxt <jamilxt@gmail.com>
2022-12-20 00:30:25 +0600

    initial commit

---
+++ src/main/java/mentees/jamilxt/borrowmybook/BorrowmybookApplication.java
@@ -0,0 +1,13 @@
+package mentees.jamilxt.borrowmybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowmybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowmybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mentees/jamilxt/borrowmybook/BorrowmybookApplicationTests.java
@@ -0,0 +1,13 @@
+package mentees.jamilxt.borrowmybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowmybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
