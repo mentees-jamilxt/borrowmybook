389154bcabfcecebd64c914761ff991e191c01f7
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-01-10 08:36:05 -0800

    make test case for HomeController

--- src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
+++ src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
@@ -1,7 +1,40 @@
 package mentees.jamilxt.borrowmybook.controller.web;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
+
+    @Autowired
+    private HomeController homeController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Check if Application Context starts or not
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(homeController).isNotNull();
+    }
+
+    //Check if expected view is shown or not
+    @Test
+    public void shouldReturnExpectedView() throws Exception {
+        this.mockMvc.perform(get("/"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("index"))
+                    .andDo(print());
+    }
 
 }
