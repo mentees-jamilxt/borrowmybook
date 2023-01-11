dbd448f4b5c69ffcee08f7f0cd3f468e7c48573b
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-01-11 14:17:48 +0600

    NR - Home controller Tested

--- src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
+++ src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
@@ -1,7 +1,37 @@
 package mentees.jamilxt.borrowmybook.controller.web;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.StringContains.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
+    @Autowired
+    private HomeController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void returnIndex() throws Exception{
+        //this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("index")));
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+
+    @Test
+    public void contextLoads() throws Exception{
+        assertThat(controller).isNotNull();
+    }
 }
