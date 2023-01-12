e9417d74feeb8564f7dfe926e05003d65b172d7a
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-01-12 11:55:07 +0600

    NR - Removed commented code in HomeControllerTest.java

--- src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
+++ src/test/java/mentees/jamilxt/borrowmybook/controller/web/HomeControllerTest.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.core.StringContains.containsString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,7 +21,6 @@ class HomeControllerTest {
 
     @Test
     public void returnIndex() throws Exception{
-        //this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("index")));
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
