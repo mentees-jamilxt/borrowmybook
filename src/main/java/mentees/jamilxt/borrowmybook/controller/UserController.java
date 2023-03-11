551aef4cf737caf6fed8d1eaa627b4b124a703de
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 08:03:59 -0800

    Modify create-user.html, view-users.html and UserController.java

--- src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @GetMapping
@@ -15,7 +15,7 @@ public class UserController {
         return "user/view-users";
     }
     
-    @GetMapping("/add-user")
+    @GetMapping("/create")
     public String viewCreateUserPage(Model model) {
         model.addAttribute("title", "Create User");
         return "user/create-user";
