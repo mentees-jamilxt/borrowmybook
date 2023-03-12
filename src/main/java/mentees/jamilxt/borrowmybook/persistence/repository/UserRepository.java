db9d80ce292771f3cb1692cdd93fad5d1d3f1a20
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 22:27:32 -0800

    Add UserRepository.java

---
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package mentees.jamilxt.borrowmybook.persistence.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import mentees.jamilxt.borrowmybook.persistence.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
