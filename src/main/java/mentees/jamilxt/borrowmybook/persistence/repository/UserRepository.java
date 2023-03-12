7915b9344d75d79e5c6d17ed3aff2164bf81494c
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 23:01:23 -0800

    Add UserService.java

--- src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/UserRepository.java
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package mentees.jamilxt.borrowmybook.persistence.repository;
 
+import java.awt.print.Pageable;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import mentees.jamilxt.borrowmybook.persistence.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+	Page<UserEntity> findAll(Pageable pageable);
 }
---
+++ src/main/java/mentees/jamilxt/borrowmybook/service/UserService.java
@@ -0,0 +1,40 @@
+package mentees.jamilxt.borrowmybook.service;
+
+import java.awt.print.Pageable;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import mentees.jamilxt.borrowmybook.exception.custom.NotFoundException;
+import mentees.jamilxt.borrowmybook.mapper.UserMapper;
+import mentees.jamilxt.borrowmybook.model.domain.User;
+import mentees.jamilxt.borrowmybook.model.dto.request.CreateUserRequest;
+import mentees.jamilxt.borrowmybook.persistence.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	private static final String USER_NOT_FOUND = "User not found";
+	private final UserMapper userMapper;
+	private final UserRepository userRepository;
+	
+	public Page<User> getUsers(Pageable pageable) {
+		return userRepository.findAll(pageable).map(userMapper::toDomain);
+	}
+	
+	public User getUserById(UUID id) {
+		var userEntity = userRepository.findById(id).orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+		return userMapper.toDomain(userEntity);
+	}
+	
+	public void createUser(CreateUserRequest request) {
+		var userEntity = userMapper.toEntity(request);
+		userRepository.save(userEntity);
+	}
+	
+	public void deleteUser(UUID id) {
+		userRepository.deleteById(id);
+	}
+}
