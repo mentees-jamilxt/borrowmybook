fa97072b131813b799f3e2a5de7a40c6f2286d69
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 20:50:43 -0800

    Modify UserController.java

--- src/main/java/mentees/jamilxt/borrowmybook/BorrowMyBookApplication.java
+++ src/main/java/mentees/jamilxt/borrowmybook/BorrowMyBookApplication.java
@@ -2,6 +2,12 @@ package mentees.jamilxt.borrowmybook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import mentees.jamilxt.borrowmybook.mapper.BookMapper;
+import mentees.jamilxt.borrowmybook.model.domain.Book;
+import mentees.jamilxt.borrowmybook.model.dto.request.CreateBookRequest;
+import mentees.jamilxt.borrowmybook.persistence.entity.BookEntity;
 
 @SpringBootApplication
 public class BorrowMyBookApplication {
--- src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
@@ -1,24 +1,26 @@
 package mentees.jamilxt.borrowmybook.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/users")
 public class UserController {
 
     @GetMapping
-    public String getUsers(Model model) {
-        model.addAttribute("title", "View Users");
-        return "user/view-users";
+    public ModelAndView getUsers() {
+    	var modelAndView = new ModelAndView("user/view-users");
+    	modelAndView.addObject("title", "View Users");
+        return modelAndView;
     }
     
     @GetMapping("/create")
-    public String viewCreateUserPage(Model model) {
-        model.addAttribute("title", "Create User");
-        return "user/create-user";
+    public ModelAndView viewCreateUserPage() {
+    	var modelAndView = new ModelAndView("user/create-user");
+        modelAndView.addObject("title", "Create User");
+        return modelAndView;
     }
     
 }
