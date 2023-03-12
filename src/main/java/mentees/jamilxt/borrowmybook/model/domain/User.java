8936dabcf3849f403ea008525298d92322fdb03c
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 21:03:52 -0800

    Add domain User.java

---
+++ src/main/java/mentees/jamilxt/borrowmybook/model/domain/User.java
@@ -0,0 +1,12 @@
+package mentees.jamilxt.borrowmybook.model.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+	private String firstName;
+	private String lastName;
+	private String email;
+}
