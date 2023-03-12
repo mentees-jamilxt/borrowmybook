e1569219f860721ffd0b840206748af521792317
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-11 21:05:22 -0800

    Modify domain User.java

--- src/main/java/mentees/jamilxt/borrowmybook/model/domain/User.java
+++ src/main/java/mentees/jamilxt/borrowmybook/model/domain/User.java
@@ -1,11 +1,14 @@
 package mentees.jamilxt.borrowmybook.model.domain;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class User {
+	private UUID id;
 	private String firstName;
 	private String lastName;
 	private String email;
