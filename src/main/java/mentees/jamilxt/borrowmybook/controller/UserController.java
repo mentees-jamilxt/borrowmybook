ea6d0d58354bb265980ff50fa1cbcd8a22306ebb
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-13 02:26:35 -0700

    Add view-single-user.html to view a single user

--- src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/UserController.java
@@ -7,11 +7,10 @@ import mentees.jamilxt.borrowmybook.service.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,6 +24,15 @@ public class UserController {
     	modelAndView.addObject("title", "View Users");
         Page<User> users = userService.getUsers(Pageable.unpaged());
         modelAndView.addObject("users", users);
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/{name}")
+    public ModelAndView getUserById(@PathVariable UUID id) {
+        var modelAndView = new ModelAndView("user/view-single-user");
+        modelAndView.addObject("title", "User Profile");
+        User user = userService.getUserById(id);
+        modelAndView.addObject("user", user);
         return modelAndView;
     }
     
