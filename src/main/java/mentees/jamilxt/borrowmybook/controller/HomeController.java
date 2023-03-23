77b6cc1f1d3084f100422a4dc59272a5ded10472
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-03-24 01:01:16 +0600

    Dashboard title add for tab

--- src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
@@ -2,6 +2,7 @@ package mentees.jamilxt.borrowmybook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,8 +18,10 @@ public class HomeController {
     }
 
     @GetMapping("/after-login-dashboard")
-    public String afterLogin() {
-        return "dashboard/index";
+    public ModelAndView afterLoginAdminPanel() {
+        var modelAndView = new ModelAndView("dashboard/index");
+        modelAndView.addObject("pageTitle", "Dashboard");
+        return modelAndView;
     }
 
 }
