74d0e08efb245eda935b8decee73f75ab7ffc387
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-03-24 12:23:21 +0600

    Bugfix Update book.

--- src/main/java/mentees/jamilxt/borrowmybook/controller/book/BookController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/book/BookController.java
@@ -6,7 +6,6 @@ import mentees.jamilxt.borrowmybook.model.dto.request.CreateBookRequest;
 import mentees.jamilxt.borrowmybook.service.BookService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +16,7 @@ import java.util.UUID;
 @Controller
 @RequestMapping(value = "/books")
 public class BookController {
+
     private final BookService bookService;
 
     @GetMapping
@@ -32,7 +32,7 @@ public class BookController {
 
     @GetMapping("create")
     public ModelAndView createBookPage() {
-        var modelAndView = new ModelAndView("/book/new-book");
+        var modelAndView = new ModelAndView("book/new-book");
         var createBookRequest = new CreateBookRequest();
         modelAndView.addObject("book", createBookRequest);
         modelAndView.addObject("pageTitle", "Book Add");
@@ -47,17 +47,18 @@ public class BookController {
 
     @GetMapping("{id}/update")
     public ModelAndView updateBookPage(@PathVariable UUID id) {
-        var mav = new ModelAndView("new-book");
+        var mav = new ModelAndView("book/update-book");
         var book = bookService.getBook(id);
         mav.addObject("book", book);
+        mav.addObject("pageTitle", "Role Update");
         return mav;
     }
 
-//    @PostMapping
-//    public String updateBook(@ModelAttribute CreateBookRequest request) {
-//        bookService.updateBook(request);
-//        return "redirect:/books";
-//    }
+    @PostMapping("update")
+    public String updateBook(@ModelAttribute CreateBookRequest request) {
+        bookService.updateBook(request);
+        return "redirect:/books";
+    }
 
     @GetMapping("{id}/delete")
     public String deleteBook(@PathVariable UUID id) {
--- src/main/java/mentees/jamilxt/borrowmybook/service/BookService.java
+++ src/main/java/mentees/jamilxt/borrowmybook/service/BookService.java
@@ -5,13 +5,11 @@ import mentees.jamilxt.borrowmybook.exception.custom.NotFoundException;
 import mentees.jamilxt.borrowmybook.mapper.BookMapper;
 import mentees.jamilxt.borrowmybook.model.domain.Book;
 import mentees.jamilxt.borrowmybook.model.dto.request.CreateBookRequest;
-import mentees.jamilxt.borrowmybook.model.enums.BookStatus;
 import mentees.jamilxt.borrowmybook.persistence.repository.BookRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -32,6 +30,18 @@ public class BookService {
 
     public void createBook(CreateBookRequest request) {
         var bookEntity = bookMapper.toEntity(request);
+        bookRepository.save(bookEntity);
+    }
+
+    public void updateBook(CreateBookRequest request) {
+        var bookEntity = bookRepository.findById(request.getId()).orElseThrow(() -> new NotFoundException(BOOK_NOT_FOUND));
+        bookEntity.setName(request.getName());
+        bookEntity.setAuthor(request.getAuthor());
+        bookEntity.setPrice(request.getPrice());
+        bookEntity.setPublisher(request.getPublisher());
+        bookEntity.setEdition(request.getEdition());
+        bookEntity.setStatus(request.getStatus());
+        bookEntity.setIsbnNumber(request.getIsbnNumber());
         bookRepository.save(bookEntity);
     }
 
