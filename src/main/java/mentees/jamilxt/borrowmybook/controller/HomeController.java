5ced49e8dc70f78b673d104593df76062659a16e
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-02 10:23:21 -0700

    Modify HomeController.java

--- src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
@@ -1,11 +1,26 @@
 package mentees.jamilxt.borrowmybook.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.RequiredArgsConstructor;
+import mentees.jamilxt.borrowmybook.model.domain.User;
+import mentees.jamilxt.borrowmybook.service.UserService;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+	
+	private final UserService userService;
+	
+	public void loadCommonData(ModelAndView modelAndView, Principal principal) {
+		String username = principal.getName();
+		User user = userService.getUserByUsername(username);
+		modelAndView.addObject("user", user);
+	}
 
     @GetMapping
     public String home() {
@@ -18,8 +33,9 @@ public class HomeController {
     }
 
     @GetMapping("/after-login-dashboard")
-    public ModelAndView afterLoginAdminPanel() {
+    public ModelAndView afterLoginAdminPanel(Principal principal) {
         var modelAndView = new ModelAndView("dashboard/index");
+        loadCommonData(modelAndView, principal);
         modelAndView.addObject("pageTitle", "Dashboard");
         return modelAndView;
     }
