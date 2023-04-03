a8f2fcd9d07b6d7e5a5c398b90669ea674731892
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-03 02:16:42 -0700

    Add validation in CreateRoleRequest.java

--- src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateRoleRequest.java
+++ src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateRoleRequest.java
@@ -4,11 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.UUID;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class CreateRoleRequest {
     private UUID id;
+    
+    @NotEmpty(message = "Role name is required.")
+    @Size(min = 4, max = 15, message = "Role name must be between 4 to 15 characters.")
     private String name;
+    
+    @NotEmpty(message = "Role description is required")
+    @Size(message = "Role description must be between 10 to 1000 characters.")
     private String description;
 
 }
