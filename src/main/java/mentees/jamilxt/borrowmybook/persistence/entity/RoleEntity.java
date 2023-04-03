340c1e3dacb39a546204712bd91d0d86048087b7
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-03 02:18:14 -0700

    Change column length for description in RoleEntity.java

--- src/main/java/mentees/jamilxt/borrowmybook/persistence/entity/RoleEntity.java
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/entity/RoleEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import mentees.jamilxt.borrowmybook.constant.EntityConstant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -13,5 +14,7 @@ import javax.persistence.Table;
 @Table(name = EntityConstant.ROLE)
 public class RoleEntity extends BaseEntity{
     private String name;
+    
+    @Column(length = 1000)
     private String description;
 }
