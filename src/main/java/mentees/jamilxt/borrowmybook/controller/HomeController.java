05117b9aabbd04bd5b20a5bf501944e0e74c97de
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-04 23:42:27 -0700

    Modify AdminController.java

--- src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/HomeController.java
@@ -3,24 +3,16 @@ package mentees.jamilxt.borrowmybook.controller;
 import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.RequiredArgsConstructor;
-import mentees.jamilxt.borrowmybook.model.domain.User;
 import mentees.jamilxt.borrowmybook.service.UserService;
 
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 	private final UserService userService;
-	
-	public void loadUserDetails(Model model, Principal principal) {
-		String username = principal.getName();
-		User loggedInUser = userService.getUserByUsername(username);
-		model.addAttribute("loggedInUser", loggedInUser);
-	}
 
     @GetMapping
     public String home() {
@@ -33,10 +25,10 @@ public class HomeController {
     }
 
     @GetMapping("/after-login-dashboard")
-    public ModelAndView afterLoginAdminPanel(Model model, Principal principal) {
+    public ModelAndView afterLoginAdminPanel(Principal principal) {
         var modelAndView = new ModelAndView("dashboard/index");
-        loadUserDetails(model, principal);
         modelAndView.addObject("pageTitle", "Dashboard");
+        modelAndView.addObject("loggedInUser", userService.getLoggedInUser(principal));
         return modelAndView;
     }
 
