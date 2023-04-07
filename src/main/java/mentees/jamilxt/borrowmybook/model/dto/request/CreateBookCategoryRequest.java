a3ad22f6ce4e6a423cbdcfc8ae68fabb60c90467
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-04-07 22:32:11 +0600

    Added attributes in the class

--- src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
+++ src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
@@ -1,2 +1,14 @@
-package mentees.jamilxt.borrowmybook.model.dto.request;public class CreateBookCategoryRequest {
+package mentees.jamilxt.borrowmybook.model.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+public class CreateBookCategoryRequest {
+    private UUID categoryId;
+    private String categoryName;
+    private String categoryDesc;
 }
