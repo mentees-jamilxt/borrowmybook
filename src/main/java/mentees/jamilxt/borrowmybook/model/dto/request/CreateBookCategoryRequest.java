cfff4d821455c75ba201aabc8925724edc946695
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-04-08 12:50:50 +0600

    Added @Repository Tag in BookCategoryRepository Created BookCategoryService with CRUD methods Added books attributes in CreateBookCategoryRequest

--- src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
+++ src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
@@ -2,7 +2,10 @@ package mentees.jamilxt.borrowmybook.model.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import mentees.jamilxt.borrowmybook.model.domain.Book;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Setter
@@ -11,5 +14,6 @@ public class CreateBookCategoryRequest {
     private UUID categoryId;
     private String categoryName;
     private String categoryDesc;
+    private List<Book> books = new ArrayList<>();
 
 }
--- src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/BookCategoryRepository.java
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/BookCategoryRepository.java
@@ -2,8 +2,10 @@ package mentees.jamilxt.borrowmybook.persistence.repository;
 
 import mentees.jamilxt.borrowmybook.persistence.entity.BookCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategoryEntity, UUID> {
 }
