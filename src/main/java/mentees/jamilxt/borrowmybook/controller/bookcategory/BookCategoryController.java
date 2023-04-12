d60c0c67c32a0e46f9cce33af07d727a8d2c85eb
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-12 12:43:37 -0700

    Add flash message section in new-category.html and implemented validation for creating new book category

--- src/main/java/mentees/jamilxt/borrowmybook/controller/bookcategory/BookCategoryController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/bookcategory/BookCategoryController.java
@@ -2,12 +2,20 @@ package mentees.jamilxt.borrowmybook.controller.bookcategory;
 
 import java.security.Principal;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
+import mentees.jamilxt.borrowmybook.helper.ResponseMessage;
 import mentees.jamilxt.borrowmybook.model.dto.request.CreateBookCategoryRequest;
 import mentees.jamilxt.borrowmybook.service.UserService;
 
@@ -25,5 +33,28 @@ public class BookCategoryController {
 		var createBookCategoryRequest = new CreateBookCategoryRequest(); 
 		modelAndView.addObject("bookCategory", createBookCategoryRequest);
 		return modelAndView;
+	}
+	
+	@PostMapping
+	public String createBookCategory(
+		@Valid @ModelAttribute("bookCategory") CreateBookCategoryRequest request,
+		BindingResult bindingResult,
+		RedirectAttributes redirectAttributes,
+		Model model,
+		Principal principal
+	) {
+		try {
+			if(bindingResult.hasErrors()) {
+				model.addAttribute("pageTitle", "Add Category");
+				model.addAttribute("loggedInUser", userService.getLoggedInUser(principal));
+				model.addAttribute("bookCategory", request);
+				return "bookCategory/new-category";
+			}
+			return "redirect:/book-categories";
+		} 
+		catch (Exception e) {
+			redirectAttributes.addFlashAttribute("responseMessage", new ResponseMessage("alert-danger", "Something went wrong. "+e.getMessage()));
+			return "redirect:/book-categories/create";
+		}
 	}
 }
