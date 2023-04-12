25ee070e5483636fe33d35946719f138f46d013a
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-12 12:15:53 -0700

    Add validation constraint in CreateBookCategoryRequest.java

--- src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
+++ src/main/java/mentees/jamilxt/borrowmybook/model/dto/request/CreateBookCategoryRequest.java
@@ -2,18 +2,23 @@ package mentees.jamilxt.borrowmybook.model.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import mentees.jamilxt.borrowmybook.model.domain.Book;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Setter
 @Getter
 public class CreateBookCategoryRequest {
     private UUID id;
+    
+    @NotEmpty(message = "Category name is required.")
+    @Size(min = 4, max = 50, message = "Category name must be between 4 to 50 characters.")
     private String name;
+    
+    @NotEmpty(message = "Category description is required.")
+    @Size(min = 10, max = 1000, message = "Category description must be between 10 to 1000 characters.")
     private String description;
-    private List<Book> books = new ArrayList<>();
 
 }
