f95a2162d73905a6b32ef65bc93231f8a4c85531
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-12 11:00:54 -0700

    Create new package controller/bookcategory, add BookCategoryController.java and add endpoint

---
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/bookcategory/BookCategoryController.java
@@ -0,0 +1,10 @@
+package mentees.jamilxt.borrowmybook.controller.bookcategory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/book-categories")
+public class BookCategoryController {
+
+}
