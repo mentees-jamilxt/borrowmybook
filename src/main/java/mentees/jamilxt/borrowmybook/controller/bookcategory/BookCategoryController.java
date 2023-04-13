0d31d3f6bdb0fff047cb7359b61cf7b1480b65e4
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-13 03:05:56 -0700

    Add method getBookCategories() in BookCategoryController.java

--- src/main/java/mentees/jamilxt/borrowmybook/controller/bookcategory/BookCategoryController.java
+++ src/main/java/mentees/jamilxt/borrowmybook/controller/bookcategory/BookCategoryController.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
+import mentees.jamilxt.borrowmybook.constant.AppConstant;
 import mentees.jamilxt.borrowmybook.helper.ResponseMessage;
+import mentees.jamilxt.borrowmybook.model.domain.BookCategory;
 import mentees.jamilxt.borrowmybook.model.dto.request.CreateBookCategoryRequest;
 import mentees.jamilxt.borrowmybook.service.BookCategoryService;
 import mentees.jamilxt.borrowmybook.service.UserService;
@@ -26,6 +31,18 @@ import mentees.jamilxt.borrowmybook.service.UserService;
 public class BookCategoryController {
 	private final UserService userService;
 	private final BookCategoryService bookCategoryService;
+	
+	@GetMapping
+	public ModelAndView getBookCategories(@RequestParam(defaultValue = "0") int page, Principal principal) {
+		var modelAndView = new ModelAndView("/bookcategory/list");
+		modelAndView.addObject("pageTitle", "View Book Categories");
+		modelAndView.addObject("loggedInUser", userService.getLoggedInUser(principal));
+		Page<BookCategory> bookCategories = bookCategoryService.getBookCategories(PageRequest.of(page, AppConstant.DEFAULT_PAGE_SIZE));
+		modelAndView.addObject("bookCategories", bookCategories);
+		modelAndView.addObject("pagesForPagination", bookCategories);
+		modelAndView.addObject("url", "/book-categories");
+		return modelAndView;
+	}
 	
 	@GetMapping("/create")
 	public ModelAndView viewCreateBookCategoryPage(Principal principal) {
