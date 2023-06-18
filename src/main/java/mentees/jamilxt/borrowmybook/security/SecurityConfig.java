9c5bd3a982def054183b2b289bbb06d0ae695352
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-06-18 23:40:15 +0600

    Delete unnecessary code.

--- src/main/java/mentees/jamilxt/borrowmybook/security/SecurityConfig.java
+++ src/main/java/mentees/jamilxt/borrowmybook/security/SecurityConfig.java
@@ -5,12 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -76,17 +73,7 @@ public class SecurityConfig {
                 .tokenValiditySeconds(DEFAULT_TOKEN_VALIDITY_SECONDS);
 
         http.addFilterAfter(new LoggedInUserFilter(), UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
-
     }
 
-//    @Bean
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/css/**");
-//        web.ignoring().antMatchers("/scripts/**");
-//        web.ignoring().antMatchers("/images/**");
-//        web.ignoring().antMatchers("/js/**");
-//        web.ignoring().antMatchers("/webjars/**");
-//    }
 }
