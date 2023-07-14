3f39596aa8946ef5453f7b629b17037dc7c9f801
meheedihasaan <mehedihasan111941@gmail.com>
2023-07-14 15:05:04 +0600

    Modify UserRepository.java

--- src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/UserRepository.java
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/repository/UserRepository.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import mentees.jamilxt.borrowmybook.persistence.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID>, JpaSpecificationExecutor<UserEntity> {
     Optional<UserEntity> findByEmail(String email);
 
     Boolean existsByEmail(String email);
