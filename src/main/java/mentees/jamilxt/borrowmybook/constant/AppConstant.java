98d0c8ec5f9e0784dd0c0191ad26f00addd85e34
meheedihasaan <mehedihasan111941@gmail.com>
2023-07-14 16:02:39 +0600

    Modify AppConstant.java

--- src/main/java/mentees/jamilxt/borrowmybook/constant/AppConstant.java
+++ src/main/java/mentees/jamilxt/borrowmybook/constant/AppConstant.java
@@ -12,7 +12,14 @@ public final class AppConstant {
     public static final String CONSUMER_ROLE_DESCRIPTION = "Role for general user.";
     public static final String DEFAULT_SYSTEM = "SYSTEM";
     public static final String ANONYMOUS_USER = "ANONYMOUS";
+    public static final String PAGE_NO = "pageNo";
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String SORT_BY = "sortBy";
+    public static final String ASC_OR_DESC = "ascOrDesc";
+    public static final String DEFAULT_PAGE_NO = "0";
     public static final int DEFAULT_PAGE_SIZE = 10;
+    public static final String DEFAULT_SORT_BY_VALUE = "createdAt";
+    public static final String DEFAULT_ASC_OR_DESC_VALUE = "DESC";
     public static final int DEFAULT_TOKEN_VALIDITY_SECONDS = 7 * 24 * 60 * 60; // 7 DAY
 
     private AppConstant() {
