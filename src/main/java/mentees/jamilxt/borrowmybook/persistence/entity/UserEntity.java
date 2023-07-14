9193322d029941d6e6f9e22044c83a57d9410ccd
meheedihasaan <mehedihasan111941@gmail.com>
2023-07-14 12:31:50 +0600

    Add two new field in UserEntity.java

--- src/main/java/mentees/jamilxt/borrowmybook/persistence/entity/UserEntity.java
+++ src/main/java/mentees/jamilxt/borrowmybook/persistence/entity/UserEntity.java
@@ -19,7 +19,8 @@ public class UserEntity extends BaseEntity {
 	private String lastName;
 	private String email;
 	private String password;
-	private boolean isEnable;
+	private Boolean isVerified = false;
+	private Boolean isBanned = false;
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "users_roles",
